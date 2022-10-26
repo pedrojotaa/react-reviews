415e6575a4e426f067452c99aae778a555bf7edf
“João <eng.joao.pedro@outlook.com>
2022-10-26 20:43:25 -0300

    first commit

---
+++ src/Reviews.js
@@ -0,0 +1,37 @@
+import styles from './Reviews.module.css'
+
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+import people from "./data";
+
+
+function Reviews() {
+  const [index, setIndex] = useState(0);
+  const { id, name, job, image, text } = people[index];
+
+  return (
+    <article className={styles.review}>
+      <div className={styles.image_container}>
+        <img src={image} alt={name} className={styles.image_person}></img>
+        <span className={styles.quote_icon}>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4>{name}</h4>
+      <p>{job}</p>
+      <p>{text}</p>
+      <div>
+        <button>
+          <FaChevronLeft />
+        </button>
+        <button>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button>suprise me</button>
+    </article>
+  );
+}
+
+export default Reviews;
